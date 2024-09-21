84e3e0919d9bfbccabe8b03a5a08fc189e5aa101
joaovicttorsmelo <joaovicttorsilveiramelo@gmail.com>
2024-09-21 13:01:03 -0300

    Removi a validação por data atual, pois não tem necessidade

--- automatizacao.py
+++ automatizacao.py
@@ -1,6 +1,5 @@
 import os
 import time
-
 import paramiko
 import yagmail
 from datetime import datetime
@@ -33,11 +32,6 @@ smtp_username = dados['smtp']['username']
 smtp_password = dados['smtp']['password']
 
 while True:
-    # Função para verificar a data de modificação do arquivo
-    def verificar_data_modificacao(arquivo):
-        data_modificacao = datetime.fromtimestamp(os.path.getmtime(arquivo))
-        return data_modificacao.date() == datetime.today().date()
-
 
     # Conexão SFTP
     def conectar_sftp():
@@ -45,7 +39,6 @@ while True:
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname, port, username, password)
         return ssh.open_sftp()
-
 
     # Função para verificar se o nome do arquivo já está no log
     def verificar_arquivo_no_log(nome_arquivo):
@@ -58,7 +51,6 @@ while True:
                         return True
         return False
 
-
     # Função para verificar se o arquivo já existe no servidor SFTP
     def verificar_arquivo_existe(sftp, nome_arquivo, diretorio):
         try:
@@ -67,13 +59,11 @@ while True:
         except FileNotFoundError:
             return False
 
-
     # Função para registrar mensagens no log
     def registrar_log(mensagem):
         with open(log_file_path, 'a') as log_file:
             data_hora = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             log_file.write(f"[{data_hora}] {mensagem}\n")
-
 
     # Função para enviar um e-mail consolidado com todos os arquivos enviados
     def enviar_email_consolidado(arquivos_enviados):
@@ -101,31 +91,27 @@ while True:
             print(f"Erro ao enviar o e-mail: {e}")
             registrar_log(f"Erro ao enviar o e-mail: {e}")
 
-
     # Função principal
     def listar_e_transferir_arquivos():
         try:
             # Conectando ao servidor SFTP
             sftp = conectar_sftp()
 
-            # Listando os arquivos .ret modificados hoje
+            # Listando os arquivos .ret
             arquivos_para_transferir = []
             arquivos_enviados = []
             for arquivo in os.listdir(local_directory):
                 if arquivo.endswith('.ret'):
-                    caminho_arquivo = os.path.join(local_directory, arquivo)
-
-                    if verificar_data_modificacao(caminho_arquivo):
-                        # Verificar se o arquivo já foi enviado pelo log
-                        if verificar_arquivo_no_log(arquivo):
-                            print(f"O arquivo {arquivo} já foi enviado anteriormente. Não será transferido novamente.")
-                            registrar_log(f"Arquivo {arquivo} já existe no log. Transferência não realizada.")
-                        else:
-                            arquivos_para_transferir.append(arquivo)
+                    # Verificar se o arquivo já foi enviado pelo log
+                    if verificar_arquivo_no_log(arquivo):
+                        print(f"O arquivo {arquivo} já foi enviado anteriormente. Não será transferido novamente.")
+                        registrar_log(f"Arquivo {arquivo} já existe no log. Transferência não realizada.")
+                    else:
+                        arquivos_para_transferir.append(arquivo)
 
             # Listando arquivos encontrados
             if arquivos_para_transferir:
-                print("Arquivos modificados hoje encontrados:")
+                print("Arquivos encontrados:")
                 for arquivo in arquivos_para_transferir:
                     print(f"- {arquivo}")
 
@@ -142,20 +128,18 @@ while True:
                             registrar_log(f"Arquivo {arquivo} enviado com sucesso.")
                             arquivos_enviados.append(arquivo)
                         else:
-                            print(
-                                f"Arquivo {arquivo} já existe em {remote_directory_in} ou {remote_directory_done}, não foi transferido.")
-                            registrar_log(
-                                f"Arquivo {arquivo} já existe em {remote_directory_in} ou {remote_directory_done}, não foi transferido.")
+                            print(f"Arquivo {arquivo} já existe em {remote_directory_in} ou {remote_directory_done}, não foi transferido.")
+                            registrar_log(f"Arquivo {arquivo} já existe em {remote_directory_in} ou {remote_directory_done}, não foi transferido.")
                     except Exception as e:
                         print(f"Erro ao transferir o arquivo {arquivo}: {e}")
                         registrar_log(f"Erro ao transferir o arquivo {arquivo}: {e}")
 
                 # Enviar um único e-mail consolidado com os arquivos enviados
                 if arquivos_enviados:
-                        enviar_email_consolidado(arquivos_enviados)
+                    enviar_email_consolidado(arquivos_enviados)
             else:
-                print("Nenhum arquivo .ret modificado hoje foi encontrado.")
-                registrar_log("Nenhum arquivo .ret modificado hoje foi encontrado.")
+                print("Nenhum arquivo .ret foi encontrado.")
+                registrar_log("Nenhum arquivo .ret foi encontrado.")
 
             # Fechando a conexão SFTP
             sftp.close()
@@ -164,7 +148,7 @@ while True:
             print(f"Ocorreu um erro: {e}")
             registrar_log(f"Ocorreu um erro geral: {e}")
 
-
     # Executar a função
     listar_e_transferir_arquivos()
+    # programa espera 30 min para rodar novamente
     time.sleep(1800)
